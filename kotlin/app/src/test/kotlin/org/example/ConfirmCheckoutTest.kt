93e79c2dc1e2c84809d04b96d8db07374d22c0d4
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2024-03-05 20:01:43 +0100

    stubs, spies and mocks

--- kotlin/app/src/test/kotlin/org/example/ConfirmCheckoutTest.kt
+++ kotlin/app/src/test/kotlin/org/example/ConfirmCheckoutTest.kt
@@ -1,5 +1,8 @@
 package org.example
 
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Test
 import java.util.Hashtable
 import java.util.UUID
@@ -49,7 +52,19 @@ class ConfirmCheckoutTest {
 
     @Test
     fun stub() {
-        val purchaseOrderRepository = mock()
+        val purchaseOrderRepository = mockk<PurchaseOrderRepository>()
+
+        val purchase = Purchase(
+            id = UUID.randomUUID(),
+            customerId = "asdsad",
+            productAndQuantityPairs = listOf(),
+            shippingAddress = "sads"
+        )
+
+        every {
+            purchaseOrderRepository.save(purchase = purchase)
+        } returns purchase
+
         ConfirmCheckout(
             uuidGenerator = UUIDGenerator(),
             purchaseOrderRepository = purchaseOrderRepository,
@@ -61,11 +76,32 @@ class ConfirmCheckoutTest {
         )
     }
 
-    class FakePurchaseOrderRepository: PurchaseOrderRepository {
-        private val hashtable = Hashtable<UUID, Purchase>()
-        override fun save(purchase: Purchase): Purchase {
-            hashtable[purchase.id] = purchase
-            return purchase
-        }
+    @Test
+    fun `spies and mocks`() {
+        val purchaseOrderRepository = mockk<PurchaseOrderRepository>()
+
+        val purchase = Purchase(
+            id = UUID.randomUUID(),
+            customerId = "asdsad",
+            productAndQuantityPairs = listOf(),
+            shippingAddress = "sads"
+        )
+
+        every {
+            purchaseOrderRepository.save(purchase = purchase)
+        } returns purchase
+
+        ConfirmCheckout(
+            uuidGenerator = UUIDGenerator(),
+            purchaseOrderRepository = purchaseOrderRepository,
+            pricingService = TODO(),
+            paymentService = TODO(),
+            invoicingService = TODO(),
+            shippingService = TODO(),
+            stockService = TODO()
+        )
+
+        verify { purchaseOrderRepository.save(purchase) }
     }
+
 }
